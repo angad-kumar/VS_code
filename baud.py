20c6490a90d2a6a6630af6bfd57158230fd98ca1
Angad Kumar <angad9745@gmail.com>
2021-10-07 18:36:53 +0530

    test_1

---
+++ baud.py
@@ -0,0 +1,8 @@
+clock = 32000000
+usartdiv = 32000000/(16 * 9600)
+
+print(usartdiv)
+
+dec = 0.33333333333334
+fraction = 16 * dec
+print(fraction)
---
+++ random_number.py
@@ -0,0 +1,9 @@
+# import random
+# randomlist = []
+# for i in range(0,100):
+#     n = random.randint(12,30)
+#     randomlist.append(n)
+# print(randomlist)
+
+for i in range(0,100):
+    print(str(i) + ",")
